a6cc368ea69bd906153ec0134a6d0d0a468c2c81
PeachSquared <contact@peachsquared.com>
2026-04-15 19:58:50 +0200

    Initial Commit

---
+++ BuddyGolf Watch App/BuddyGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyGolfApp.swift
+//  BuddyGolf Watch App
+//
+//  Created by Chris Stappen on 15/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BuddyGolf_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
